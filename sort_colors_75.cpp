738f198244f2bd3f72089682a8c588cf7d9bb078
Ashish-Mahendrakar <ashish.mahendrakar2268@gmail.com>
2025-11-03 16:35:52 +0530

    leetcode solution 75

--- sort_colors_75.cpp
+++ sort_colors_75.cpp
@@ -42,3 +42,32 @@ public:
         
     }
 };
+
+
+// second solution
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int zero=0;
+//         int two=nums.size()-1;
+//         int size=two+1;
+//         int i=0;
+//         while (zero<= i && i<=two && i<size && zero<size && two>=0)
+//         {
+//             if(nums[i]==0)
+//             {
+//                 swap(nums[i],nums[zero]);
+//                 zero++;
+                
+//             }
+//             else if(nums[i]==2)
+//             {
+//                 swap(nums[i],nums[two]);
+//                 two--;
+//                 continue;
+//             }
+//             i++;
+//         }
+        
+//     }
+// };
