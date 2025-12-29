ab21381e48909dc74f1fc2c793345870fcdde2e9
Ashish-Mahendrakar <ashish.mahendrakar2268@gmail.com>
2025-12-29 16:31:58 +0530

    solution 210

---
+++ course_schedule_2_210.cpp
@@ -0,0 +1,79 @@
+class graph{
+    public:
+        unordered_map <int,list<int>> adjlist;
+        void insert(int a,int b)
+        {
+            adjlist[b].push_back(a);
+        }
+        void printt()
+        {
+            for(auto i : adjlist)
+            {
+                cout<<i.first<<"->";
+                for(auto j : i.second)
+                {
+                    cout<<j<<" ";
+                }
+                cout<<endl;
+            }
+        }
+        void bfs(int &numCourses,vector<int> &topsort)
+        {
+            unordered_map <int,int> indegree;
+            queue <int> q;
+            for(int i=0;i<numCourses;i++)
+            {
+                indegree[i]=0;
+            }
+            for(auto i : adjlist)
+            {
+                
+                for(auto j :i.second)
+                     indegree[j]++;
+            }
+
+            for(auto i :indegree)
+            {
+                if(i.second == 0)
+                    q.push(i.first);
+
+            }
+            while(!q.empty())
+            {
+                int cur=q.front();
+                q.pop();
+                topsort.push_back(cur);
+                for(auto nebr: adjlist[cur])
+                {
+                    indegree[nebr]--;
+                    if(indegree[nebr]==0)
+                    {
+                        q.push(nebr);
+                    }
+                }
+            }
+        }
+
+};
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        graph g;
+        
+        for(auto i : prerequisites)
+        {
+            g.insert(i[0],i[1]);
+            
+        }
+        //g.printt();
+        vector <int> topsort;
+        g.bfs(numCourses,topsort);
+        
+        cout<<topsort.size();
+        if(topsort.size()==numCourses)
+            return topsort;
+
+        vector <int> temp;
+        return temp;
+    }
+};
