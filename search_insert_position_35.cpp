0da293c45c282d20b1c5173d38aeeaa36489038e
Ashish-Mahendrakar <ashish.mahendrakar2268@gmail.com>
2026-01-16 16:10:17 +0530

    solution 35

---
+++ search_insert_position_35.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0;
+        int ans=-1;
+        int end=nums.size()-1;
+        while(start <= end)
+        {
+            int mid=(start+end)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]<target)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else
+            {
+
+                end=mid-1;
+            }
+        }
+        return ans+1;
+    }
+};
