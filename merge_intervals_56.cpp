828d6c04ddfe68a421ab5905c5c789af91dde9f9
Ashish-Mahendrakar <ashish.mahendrakar2268@gmail.com>
2026-01-19 22:35:38 +0530

    solution 56

---
+++ merge_intervals_56.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    vector<int> find(vector<int> a,vector<int> b)
+    {
+        if(a[0]<=b[0])
+        {
+            //cout<<"j";
+            a[1]=max(b[1],a[1]);
+            //cout<<a[0]<<" "<<a[1]<<endl;
+            return a;
+        }
+        else
+        {
+            b[1]=max(b[1],a[1]);
+            //cout<<b[0]<<" "<<b[1]<<endl;
+            return b;
+        }
+    }
+    bool check(vector<int> &a,vector<int> &b)
+    {
+        if(a[0]<=b[0])
+        {
+            //cout<<"j";
+            if(a[1]>=b[0])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if(b[1]>=a[0])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        stack <vector<int>> s;
+        if(intervals.size()==0)
+        {
+            return intervals;
+        }
+        sort(intervals.begin(),intervals.end());
+
+        //s.push(intervals[0]);
+        //cout<<"j";
+        for(int i=0;i<intervals.size();i++)
+        {
+
+            if(s.empty())
+            {
+                s.push(intervals[i]);
+                continue;
+            }
+            bool status=check(s.top(),intervals[i]);
+            //cout<<status<<endl;
+
+            if(status )
+            {
+                intervals[i]=find(s.top(),intervals[i]);
+                //cout<<intervals[i][0]<<" "<<intervals[i][1];
+                s.pop();
+                i--;
+            }
+            else
+            {
+                s.push(intervals[i]);
+            }
+            //cout<<"i"<<i<<endl;
+        }
+        //cout<<s.size();
+        vector<vector<int>> ans;
+        while(!s.empty())
+        {
+            ans.push_back(s.top());
+            s.pop();
+        }
+
+        return ans;
+    }
+};
