c041011f13dcdd69563f06d37ffd7df4c6d41311
Ashish-Mahendrakar <ashish.mahendrakar2268@gmail.com>
2026-01-23 12:01:13 +0530

    solution 88

---
+++ merge_sorted_array_88.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> ans(m+n,0);
+        
+        
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<m && j<n)
+        {
+            
+            if(nums1[i]<nums2[j])
+            {
+                ans[k]=nums1[i];
+                i++;
+                k++;
+                //cout<<i<<" "<<j<<" "<<k<<endl;
+            }
+            else
+            {
+                ans[k]=nums2[j];
+                j++;
+                k++;
+            }
+        }
+        
+
+        while(i<m)
+        {
+            ans[k]=nums1[i];
+            i++;
+            k++;
+        }
+
+        while(j<n)
+        {
+            ans[k]=nums2[j];
+            j++;
+            k++;
+        }
+        nums1=ans;
+    }
+};
