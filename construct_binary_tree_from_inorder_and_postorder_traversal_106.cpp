43b40c2cc0aa62c38990992511b74b9b3aecd2ed
Ashish-Mahendrakar <ashish.mahendrakar2268@gmail.com>
2026-01-25 19:15:56 +0530

    solution 106

---
+++ construct_binary_tree_from_inorder_and_postorder_traversal_106.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* rr(vector<int>& preorder, vector<int>& inorder,int i,int j,unordered_map <int,int> &mp)
+    {
+        
+        if(i>j)
+        {
+            //cout<<"h";
+            return NULL;
+        }
+        if(i==j)
+        {
+            
+            TreeNode* node=new TreeNode(inorder[i]);
+            return node;
+        }
+        
+
+        
+        int pi=-1;
+        int ii=-1;
+        for(int k=preorder.size()-1;k>=0;k--)
+        {
+            if(mp[preorder[k]] >=i  && mp[preorder[k]] <=j)
+            {
+                pi=k;
+                ii=mp[preorder[k]];
+                break;
+            }
+        }
+        //cout<<pi<<" "<<ii<<endl;
+
+        TreeNode * root=new TreeNode(preorder[pi]);
+        root->left=rr(preorder,inorder,i,ii-1,mp);
+        root->right=rr(preorder,inorder,ii+1,j,mp);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int i=0;
+        int j=inorder.size()-1;
+        unordered_map <int,int> mp;
+        for(int k=i;k<=j;k++)
+        {
+            mp[inorder[k]]=k;
+        }
+        TreeNode* root=rr(postorder,inorder,i,j,mp);
+        return root;
+    }
+};
